0f17942c2b694700b4c8a378af6d7fbc6a63a37c
SirBeans <ryanchung0084@gmail.com>
2024-11-23 23:15:39 -0500

    idk

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 let formElement = document.getElementById("search-form");
 
-
+const pages = [
+    { title: "Home", url: "index.html", description: "Welcome to our homepage." },
+    { title: "Template", url: "/template.html", description: "Template for the wiki pages." },
+    { title: "Goongate", url: "/goongate.html", description: "THOSE WHO KNOW" },
+    { title: "Fight Time", url: "/fight-time.html", description: "Phrase popularized by Avadhut in freshman year." },
+    { title: "Diddy Park", url: "/diddy-park.html", description: "Park near the McDonalds, consisting of a playground and Skate Park." },
+];
